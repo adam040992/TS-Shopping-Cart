8b84fb5348b6011a96502112bf69d5163887dc5f
adam040992 <adam040992@gmail.com>
2023-07-09 14:47:17 +0200

    Add: map Data

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import storeItems from "../data/items.json"
+import { Col, Row } from 'react-bootstrap'
+
 
 export function Store() {
   return (
+    <>
     <h1>Store</h1>
+    <Row lg={3} md={2} xs={1}>
+      {storeItems.map((item, index) => (
+        <Col key={index}>{JSON.stringify(item)}</Col>
+      ))}
+    </Row>
+    </>
   )
 }
 
