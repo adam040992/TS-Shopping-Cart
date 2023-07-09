9a0342ac7bae6ad850815afcad1dc4e2796ff690
adam040992 <adam040992@gmail.com>
2023-07-09 15:30:47 +0200

    Add: Storeitme

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import storeItems from "../data/items.json"
 import { Col, Row } from 'react-bootstrap'
+import { StoreItem } from '../components/StoreItem'
 
 
 export function Store() {
   return (
     <>
     <h1>Store</h1>
-    <Row lg={3} md={2} xs={1}>
-      {storeItems.map((item, index) => (
-        <Col key={index}>{JSON.stringify(item)}</Col>
+    <Row lg={3} md={2} xs={1} className='g-3'>
+      {storeItems.map((item) => (
+        <Col key={item.id}>
+          <StoreItem {...item} />
+        </Col>
       ))}
     </Row>
     </>
