109416ca4f8e6d3fb846e5c5cdafecd8a3027160
adam040992 <adam040992@gmail.com>
2023-07-15 12:14:01 +0200

    Add: Store page

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -25,7 +25,21 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
             </Card.Title>
             <div className='mt-auto'>
                 {quantity === 0 ? ( 
-                    <Button className='btn'>+ Add to Cart</Button> ) : null 
+                    <Button className='w-100'>+ Add to Cart</Button> ) : ( 
+                    <div 
+                        className='d-flex align-items-center flex-column' 
+                        style={{gap: ".5rem"}}>
+                            <div 
+                                className='d-flex align-items-center justify-content-center' 
+                                style={{gap: ".5rem"}}>
+                                    <Button>-</Button>
+                                    <div>
+                                        <span className='fs-3'>{quantity}</span> in cart
+                                    </div>
+                                    <Button>+</Button>
+                                </div>
+                        <Button variant='danger' size='sm'>Remove</Button>
+                    </div> )
                 }
             </div>
         </Card.Body>
